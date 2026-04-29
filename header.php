3e980e79c77f3e97ea6132ab9eed63a14151a16e
vegan09 <hrnmdala7220@gmail.com>
2026-04-29 21:05:40 +0200

    added filed

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+            <div class="menu">
+                    <div>
+                        <p class="app_title">KABOTOLO POS SYSTEM</p>
+                    </div>
+
+                    <div class="two">               
+                        <a href="index.php" class="menu_items">Home</a> &nbsp;
+                        <a href="sales.php" class="menu_items">Sales</a> &nbsp;
+                        <a href="inventory.php" class="menu_items">Inventory</a> &nbsp;
+                        <a href="reports.php" class="menu_items">Reports</a>
+                    </div>
+            </div>
+</body>
+</html>

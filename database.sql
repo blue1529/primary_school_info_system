e21870857649b13be7327c5497a8b2b525d59aaf
blue1529 <newtonkalulu00@gmail.com>
2026-05-05 20:32:09 +0200

    Change to the users table in database.sql, removed class and email fields

--- database.sql
+++ database.sql
@@ -80,8 +80,6 @@ CREATE TABLE grades (
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50),
-    email VARCHAR(100),
     password VARCHAR(255), -- must be hashed...
     role VARCHAR(20),   -- 'teacher'
-    class INT
 );

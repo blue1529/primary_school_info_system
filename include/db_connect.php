69802cb6f9d36086aa61ee5db75936c72aba4e2d
kaluagomezgani-ship-it <kaluagomezgani@gmail.com>
2026-05-07 00:11:22 +0200

    removed redundant files

--- include/db_connect.php
+++ include/db_connect.php
@@ -3,7 +3,7 @@
 // 1. Connect to the database (change these values to match your setup)
 $servername = "localhost"; // usually "localhost"
 $username = "root"; // your database username
-$password = "melania1529"; // your database password
+$password = ""; // your database password
 $dbname = "primary_school_info_system"; // your database name
 
 // Create connection

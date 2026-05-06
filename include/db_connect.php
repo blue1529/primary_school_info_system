a99828fb42c2227bcaa04d6f064094675110fc91
CalvinMbaula <calvinlouis360@gmail.com>
2026-05-06 23:03:14 +0200

    no changes

--- include/db_connect.php
+++ include/db_connect.php
@@ -6,7 +6,6 @@ $username = "root"; // your database username
 $password = "melania1529"; // your database password
 $dbname = "primary_school_info_system"; // your database name
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {

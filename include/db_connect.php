21b2ad46e1917d5f44c58fb4815df28d7ac55382
blue1529 <newtonkalulu00@gmail.com>
2026-05-07 03:50:47 +0200

    Password removal

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,9 +1,9 @@
 <?php 
 
 // 1. Connect to the database (change these values to match your setup)
-$servername = "localhost"; // usually "localhost"
+$servername = "localhost";
 $username = "root"; // your database username
-$password = "melania1529"; // your database password
+$password = ""; // your database password
 $dbname = "primary_school_info_system"; // your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
